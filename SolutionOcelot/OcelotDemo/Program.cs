4870c77dcdbfc9fcdf301d58ee4aced404646000
Said WAHID <said@oumdin.com>
2024-02-29 13:40:47 +0100

    Creation Projet

---
+++ SolutionOcelot/OcelotDemo/Program.cs
@@ -0,0 +1,36 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+builder.Services.AddOcelot(builder.Configuration);
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.UseOcelot();
+
+
+app.Run();
+
